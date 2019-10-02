11c6375d8f16828815ea8cc6a47456141965109b
Remi Gau <remi_gau@hotmail.com>
2019-10-02 08:23:27 +0200

    fix indent

--- subfun/InitPTB.m
+++ subfun/InitPTB.m
@@ -1,17 +1,18 @@
 function [Win, Rect, oldRes, ifi] = InitPTB(Parameters)
-screenid = Parameters.Screen;
-    
-    noScreens = length(Screen('Screens'));
-    if ismac && noScreens > 1 % only if projector is also a screen
-        oldRes = Screen('Resolution', screenid, 800,600,60);
-    end
-    
-    [Win, Rect] = Screen('OpenWindow', Parameters.Screen, Parameters.Background);
-    Screen('TextFont', Win, Parameters.FontName);
-    Screen('TextSize', Win, Parameters.FontSize);
-    Screen('BlendFunction', Win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    % interframe interval
-    ifi = Screen('GetFlipInterval', Win);
+screenid = Parameters.Screen;
+
+noScreens = length(Screen('Screens'));
+if ismac && noScreens > 1 % only if projector is also a screen
+    oldRes = Screen('Resolution', screenid, 800,600,60);
+end
+
+[Win, Rect] = Screen('OpenWindow', Parameters.Screen, Parameters.Background);
+Screen('TextFont', Win, Parameters.FontName);
+Screen('TextSize', Win, Parameters.FontSize);
+Screen('BlendFunction', Win, GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+% interframe interval
+ifi = Screen('GetFlipInterval', Win);
 
 end
