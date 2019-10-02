3893ca08aa0fcc1982540db08829aa839dd4abdd
Remi Gau <remi_gau@hotmail.com>
2019-10-02 08:51:41 +0200

    missing argument for fixation cross

--- subfun/DrawCross.m
+++ subfun/DrawCross.m
@@ -1,4 +1,4 @@
-function DrawCross(Win, Parameters, FixCrossTexture, FixCrossRect)
+function DrawCross(Win, Parameters, FixCrossTexture, fh, fw, FixCrossRect)
 if strcmp(Parameters.Apperture,'Wedge')
     % Draw the fixation cross
     Screen('DrawTexture', Win, FixCrossTexture);
