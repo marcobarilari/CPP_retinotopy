503260fcd69d76d46f2d7b1f49c9c4c26553d976
Remi Gau <remi_gau@hotmail.com>
2019-11-04 19:00:30 +0100

    Yet more refacrtoring

--- subfun/main_func/RetinotopicMapping.m
+++ subfun/main_func/RetinotopicMapping.m
@@ -39,7 +39,7 @@ end
 
 %% Event timings
 Events = CreateEventsTiming(Parameters);
-
+Behaviour.EventTime = Events;
 
 try
     
@@ -88,14 +88,14 @@ try
     CurrFrame = 1;  % Current stimulus frame
     CurrRefresh = 0;   % Current video refresh
     CurrAngle = 0;  % Current angle of wedge
-    CurrScale = 0;  % Current inner radius of ring
+    CurrScalePix = 0;  % Current inner radius of ring
     PrevKeypr = 0;
     
     if IsRing
         % currentScale is scale of outer ring (exceeding screen until inner ring reaches window boarder)
         MaxEcc = Parameters.FOV / 2 + Parameters.AppertureWidth + log(Parameters.FOV/2 + 1) ;
         % csFuncFact is used to expand with log increasing speed so that ring is at maxEcc at end of cycle
-        CsFuncFact = 1 / ((MaxEcc + exp(1)) * log(MaxEcc + exp(1)) - (MaxEcc + exp(1))) ;
+        CsFuncFact = 1 / ( (MaxEcc + exp(1)) * log(MaxEcc + exp(1)) - (MaxEcc + exp(1)) ) ;
         % Current ring width in visual angle
         CurrRingWidthVA = Parameters.AppertureWidth;
     end
@@ -106,11 +106,11 @@ try
     
     
     %% Start cycling the stimulus
-    Behaviour.EventTime = Events;
     CycleDuration = Parameters.TR * Parameters.VolsPerCycle;
     CyclingEnd = CycleDuration * Parameters.CyclesPerExpmt;
     CyclingStart = GetSecs;
-    CurrTime = GetSecs - CyclingStart;
+    CurrTime = 0;
+    CurrScaleInnerVA = 0;
     IsEvent = false;
     WasEvent = false;
     Events2 = [];
@@ -154,6 +154,7 @@ try
         
         
         %% Determine size & angle
+        
         % Rotation of apperture
         switch Parameters.Direction
             case '+'
@@ -162,44 +163,44 @@ try
                 CurrAngle = 90 - Parameters.AppertureWidth/2 - (CurrTime/CycleDuration) * 360;
         end
         
+        % CurrScale only influences  ring
         if IsRing
-            % Size of apperture (CurrScale only influences  ring)
-            if strcmpi(Parameters.Direction, '+')
-                %CurrScale = 0 + mod(CurrTime, CycleDuration)/CycleDuration * StimRect(4);
-                
-                %---tr: vary CurrScale so that expansion speed is log over eccentricity
-                % cf. Tootell 1997; Swisher 2007; Warnking 2002 etc;
-                CurrScaleVA = 0 + mod(CurrTime, CycleDuration)/CycleDuration * MaxEcc; % current visual angle linear in time
-                % ensure some foveal stimulation at beginning (which is hidden by fixation cross otherwise)
-                if CurrScaleVA < 0.5
-                    CurrScaleVA = 0.5;
-                end
-            elseif strcmpi(Parameters.Direction, '-')
-                %CurrScale = StimRect(4) - mod(CurrTime, CycleDuration)/CycleDuration * StimRect(4);
-                CurrScaleVA = MaxEcc - mod(CurrTime, CycleDuration)/CycleDuration * MaxEcc;
-                if CurrScaleVA > MaxEcc - 0.5
-                    CurrScaleVA = MaxEcc - 0.5;
-                end
+            
+            %vary CurrScale so that expansion speed is log over eccentricity
+            % cf. Tootell 1997; Swisher 2007; Warnking 2002 etc
+
+            switch Parameters.Direction
+                case '+'
+                    % current visual angle linear in time
+                    CurrScaleVA = 0 + mod(CurrTime, CycleDuration)/CycleDuration * MaxEcc; 
+                    % ensure some foveal stimulation at beginning (which is hidden by fixation cross otherwise)
+                    if CurrScaleVA < Parameters.FixationSize + .1
+                        CurrScaleVA = 0.6;
+                    end
+                case '-'
+                    CurrScaleVA = MaxEcc - mod(CurrTime, CycleDuration)/CycleDuration * MaxEcc;
+                    if CurrScaleVA > MaxEcc - 0.1
+                        CurrScaleVA = MaxEcc - 0.1;
+                    end
             end
             
             % near-exp visual angle
             CurrScaleVA2 = ((CurrScaleVA+exp(1)) * log(CurrScaleVA+exp(1)) - (CurrScaleVA+exp(1))) * MaxEcc * CsFuncFact;
-            CurrScaleCm = tan(CurrScaleVA2*pi/180)* Parameters.viewDist; % in cm  on screen
-            CurrScale = CurrScaleCm / (Parameters.xWidthScreen/2) * (StimRect(4)/2) * 2;% in pixel
-            
-            %--tr width of apperture changes logarithmically with eccentricity of inner ring, cf.
-            %authors above
-            
+            CurrScalePix = CurrScaleVA2 *PPD; % in pixel
+
+            %width of apperture changes logarithmically with eccentricity of inner ring            
             oldScaleInnerVA = CurrScaleVA - CurrRingWidthVA;
             if oldScaleInnerVA < 0
                 oldScaleInnerVA = 0;
             end
-            CurrRingWidthVA = Parameters.AppertureWidth + log(oldScaleInnerVA+1); % growing with inner ring ecc
+            
+            % growing with inner ring ecc
+            CurrRingWidthVA = Parameters.AppertureWidth + log(oldScaleInnerVA+1); 
             CurrScaleInnerVA = CurrScaleVA2 - CurrRingWidthVA;
-            CurrScaleInnerCM = tan(CurrScaleInnerVA*pi/180)* Parameters.viewDist; % in cm  on screen
-            CurrScaleInner =  2*(CurrScaleInnerCM / (Parameters.xWidthScreen/2) * (StimRect(4)/2));% in pixel
-            if CurrScaleInner < 0
-                CurrScaleInner = 0;
+            CurrScaleInnerPix =  CurrScaleInnerVA *PPD; % in pixel
+            
+            if CurrScaleInnerPix < 0
+                CurrScaleInnerPix = 0;
             end
         end
         
@@ -210,12 +211,12 @@ try
         if IsRing
             
             Screen('FillOval', AppTexture, [0 0 0 0], ...
-                CenterRectOnPoint([0 0 repmat(CurrScale,1,2)], Rect(3)/2, Rect(4)/2 ));
+                CenterRectOnPoint([0 0 repmat(CurrScalePix,1,2)], Rect(3)/2, Rect(4)/2 ));
             
             Screen('FillOval', AppTexture, [Parameters.Background 255], ...
-                CenterRectOnPoint([0 0 repmat(CurrScaleInner,1,2)], Rect(3)/2, Rect(4)/2 ));
+                CenterRectOnPoint([0 0 repmat(CurrScaleInnerPix,1,2)], Rect(3)/2, Rect(4)/2 ));
             
-            FrameTimesUpdate = [CurrTime CurrFrame CurrAngle CurrScale CurrScaleVA2 CurrScaleInner CurrScaleInnerVA];
+            FrameTimesUpdate = [CurrTime CurrFrame CurrAngle CurrScalePix CurrScaleVA2 CurrScaleInnerPix CurrScaleInnerVA];
             
         else
             
@@ -262,38 +263,32 @@ try
         
         CurrEvents = Events - CurrTime;
         
-        if all([~IsRing ; CurrEvents > 0 ; CurrEvents < Parameters.EventDuration])
+        if  all( [CurrEvents > 0 ; CurrEvents < Parameters.EventDuration] )
             IsEvent = true;
-            Events2 = [Events2, GetSecs-CyclingStart]; % for relating shown events and responses in ring runs
-        elseif  all([IsRing ; CurrScaleInnerVA > 10 ; CurrEvents > 0 ; CurrEvents < Parameters.EventDuration] )
-            IsEvent = true;
-            Events2 = [Events2, GetSecs-CyclingStart];
         else
             IsEvent = false;
         end
         
-        if IsEvent == true
+        if all( [IsRing ; CurrScaleInnerVA > 10] )
+            IsEvent = false;
+        end
+        
+        if IsEvent
             
-            if WasEvent == false
+            if ~WasEvent
                 RndAngle = RandOri;
-                % if ring is presented under wide V FOV condition, target
-                % appears only @ 0 or 180 degree (tr)
-                if IsRing
-                    possAngle = [0,180];
-                    RndAngle = possAngle(randi(2,[1 1]));
-                end
                 RndScale = round(rand*(Rect(4)/2));
                 WasEvent = true;
             end
             
             if IsRing
                 % target always on horizontal meridian
-                [X, Y] = pol2cart(RndAngle/180*pi,(CurrScale/2 + CurrScaleInner/2)/2);
+                [X, Y] = pol2cart( RndAngle/180*pi, (CurrScalePix/2 + CurrScaleInnerPix/2)/2 );
             else
-                [X, Y] = pol2cart((90 + CurrAngle + Parameters.AppertureWidth/2) / 180*pi, RndScale);
+                [X, Y] = pol2cart( (90 + CurrAngle + Parameters.AppertureWidth/2) / 180*pi, RndScale );
             end
             
-            % tr
+            % target position
             X = Rect(3)/2-X;
             Y = Rect(4)/2-Y;
             
@@ -305,7 +300,7 @@ try
                 X+EventSizePix/2 ...
                 Y+EventSizePix/2]);
             
-        elseif IsEvent == false
+        elseif ~IsEvent
             
             WasEvent = false;
             
@@ -313,6 +308,10 @@ try
         
         %% Draw current video frame
         rft = Screen('Flip', Win, rft+ifi);
+        
+        if IsEvent
+             Events2 = [Events2, rft];
+        end
         
         %% Behavioural response
         [Behaviour, PrevKeypr, QUIT] = GetBehResp(KeyCodes, Win, Parameters, Rect, PrevKeypr, Behaviour, CyclingStart);
@@ -338,7 +337,7 @@ try
     FarewellScreen(Win, Parameters, Rect)
     
     CleanUp
-    
+
     %% Save workspace
     % clear stim from structure and a few variables to save memory
     Parameters = rmfield(Parameters, 'Stimulus');
