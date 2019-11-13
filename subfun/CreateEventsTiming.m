ec1f21bcb3f9ad4dea5ca9b831ce1ac0c847432d
Remi Gau <remi_gau@hotmail.com>
2019-11-13 16:54:04 +0100

    prevent target events too close to one another

--- subfun/CreateEventsTiming.m
+++ subfun/CreateEventsTiming.m
@@ -2,6 +2,7 @@ function Events = CreateEventsTiming(Parameters)
 e = Parameters.TR : Parameters.EventDuration : (Parameters.CyclesPerExpmt * Parameters.VolsPerCycle * Parameters.TR);
 tmp = rand(length(e),1);
 Events = e(tmp < Parameters.ProbOfEvent)';
-% Add a dummy event at the end of the Universe
-Events = [Events; Inf];
+
+% remove events that are less than 1.5 seconds appart
+Events( find(diff(Events)<1)+1 ) = [];
 end
