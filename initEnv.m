ca2ae749dc1f261572c81447e9d0595d295c0afd
Remi Gau <remi_gau@hotmail.com>
2020-08-27 08:06:01 +0200

    fix intiEnv with the correct paths

--- initEnv.m
+++ initEnv.m
@@ -86,9 +86,8 @@ end
 function addDependencies()
 
     pth = fileparts(mfilename('fullpath'));
-    addpath(fullfile(pth, 'lib', 'CPP_BIDS', 'src'));
-    addpath(fullfile(pth, 'lib', 'CPP_PTB', 'src', 'aperture'));
-    addpath(fullfile(pth, 'lib', 'CPP_PTB'));
+    addpath(genpath(fullfile(pth, 'lib', 'CPP_BIDS', 'src')));
+    addpath(genpath(fullfile(pth, 'lib', 'CPP_PTB', 'src')));
     addpath(genpath(fullfile(pth, 'subfun')));
 
 end
